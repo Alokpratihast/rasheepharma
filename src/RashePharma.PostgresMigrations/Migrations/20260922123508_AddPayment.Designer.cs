0babc39130114e36016c026bccfe2e3b8ea3db6a
Alok Pratihast <alokpratihast05@gmail.com>
2026-09-22 18:08:20 +0530

    feat: add PostgreSQL payment migration

--- src/RashePharma.PostgresMigrations/Migrations/20260922123508_AddPayment.Designer.cs
+++ src/RashePharma.PostgresMigrations/Migrations/20260922123508_AddPayment.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RashePharma.Infrastructure.Data;
@@ -11,9 +12,11 @@ using RashePharma.Infrastructure.Data;
 namespace RashePharma.PostgresMigrations.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260922123508_AddPayment")]
+    partial class AddPayment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
